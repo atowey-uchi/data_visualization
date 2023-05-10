4879142e3fa7ebd96b048be6c6653b3e96ad9625
atowey-uchi <atowey@uchicago.edu>
2023-05-10 15:45:29 -0500

    updated water html

--- covid-transpo/code + data/chart3.js
+++ covid-transpo/code + data/chart3.js
@@ -8,7 +8,9 @@ var x = d3v3.time.scale().range([0, width_area]);
 
 var y = d3v3.scale.linear().range([height_area, 0]);
 
-var color = d3v3.scale.category10();
+var color = d3v3.scale
+  .ordinal()
+  .range(["#9ecae1", "#7fb3d5", "#609bc8", "#4172bc", "#08306b"]);
 
 var xAxis = d3v3.svg
   .axis()
@@ -22,24 +24,24 @@ var yAxis = d3v3.svg
   .axis()
   .scale(y)
   .orient("left")
-  .tickFormat(function(d) {
+  .tickFormat(function (d) {
     return formatValue(d).replace("G", "B");
   });
 //.tickFormat(formatPercent);
 
 var area = d3v3.svg
   .area()
-  .x(function(d) {
+  .x(function (d) {
     return x(d.date);
   })
-  .y0(function(d) {
+  .y0(function (d) {
     return y(d.y0);
   })
-  .y1(function(d) {
+  .y1(function (d) {
     return y(d.y0 + d.y);
   });
 
-var stack = d3v3.layout.stack().values(function(d) {
+var stack = d3v3.layout.stack().values(function (d) {
   return d.values;
 });
 
@@ -55,21 +57,21 @@ var svg_area = d3v3
     "translate(" + margin_area.left + "," + margin_area.top + ")"
   );
 
-d3v3.csv("data/new_distance.csv", function(error, data) {
+d3v3.csv("data/new_distance.csv", function (error, data) {
   color.domain(
-    d3v3.keys(data[0]).filter(function(key) {
+    d3v3.keys(data[0]).filter(function (key) {
       return key !== "date";
     })
   );
-  data.forEach(function(d) {
+  data.forEach(function (d) {
     d.date = parseDate(d.date);
   });
 
   var browsers = stack(
-    color.domain().map(function(name) {
+    color.domain().map(function (name) {
       return {
         name: name,
-        values: data.map(function(d) {
+        values: data.map(function (d) {
           return { date: d.date, y: d[name] * 1 };
         }),
       };
@@ -77,8 +79,8 @@ d3v3.csv("data/new_distance.csv", function(error, data) {
   );
 
   // Find the value of the day with highest total value
-  var maxDateVal = d3v3.max(data, function(d) {
-    var vals = d3v3.keys(d).map(function(key) {
+  var maxDateVal = d3v3.max(data, function (d) {
+    var vals = d3v3.keys(d).map(function (key) {
       return key !== "date" ? d[key] : 0;
     });
     return d3v3.sum(vals);
@@ -86,7 +88,7 @@ d3v3.csv("data/new_distance.csv", function(error, data) {
 
   // Set domains for axes
   x.domain(
-    d3v3.extent(data, function(d) {
+    d3v3.extent(data, function (d) {
       return d.date;
     })
   );
@@ -102,19 +104,29 @@ d3v3.csv("data/new_distance.csv", function(error, data) {
   browser
     .append("path")
     .attr("class", "area")
-    .attr("d", function(d) {
+    .attr("d", function (d) {
       return area(d.values);
     })
-    .style("fill", function(d) {
+    .style("fill", function (d) {
       return color(d.name);
+    })
+    .style("fill-opacity", 1) // Set the fill opacity for the patterns
+
+    // Add patterns to the areas
+    .style("fill", function (d, i) {
+      if (i === 0 || i === 2) {
+        return "url(#pattern-" + i + ")";
+      } else {
+        return color(d.name);
+      }
     });
 
   browser
     .append("text")
-    .datum(function(d) {
+    .datum(function (d) {
       return { name: d.name, value: d.values[d.values.length - 1] };
     })
-    .attr("transform", function(d) {
+    .attr("transform", function (d) {
       return (
         "translate(" +
         x(d.value.date) +
@@ -126,10 +138,10 @@ d3v3.csv("data/new_distance.csv", function(error, data) {
     .attr("x", 10)
     .attr("dy", "-2.05em")
     .style("font-size", "12px")
-    .style("fill", function(d) {
+    .style("fill", function (d) {
       return color(d.name);
     })
-    .text(function(d) {
+    .text(function (d) {
       return d.name;
     });
 
@@ -139,8 +151,5 @@ d3v3.csv("data/new_distance.csv", function(error, data) {
     .attr("transform", "translate(0," + height_area + ")")
     .call(xAxis);
 
-  svg_area
-    .append("g")
-    .attr("class", "y axis")
-    .call(yAxis);
+  svg_area.append("g").attr("class", "y axis").call(yAxis);
 });
